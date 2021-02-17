1ed5a0b9afcd3705f7cb5e374383d6f73e0fc94a
Kristopher Kram <kram.kristopher@gmail.com>
2021-02-17 16:05:39 +0100

    NASA

---
+++ main.js
@@ -0,0 +1,19 @@
+document.querySelector("button").onclick = function () {
+  let url = new URL(
+    "https://api.nasa.gov/planetary/apod?api_key=PCIlTkyhqe1nkB34QyF9XmZzzAj0RgkrFySr1uac"
+  );
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.responseType = "json";
+
+  xhr.onload = function () {
+    if (xhr.status != 200) {
+      alert("ERROR:");
+    } else {
+      console.log(xhr.status + " " + xhr.statusText);
+      console.log(xhr.response);
+    }
+  };
+  xhr.send();
+};
