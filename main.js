852ecece285bdf557ca87371be5671d0ac584a23
Kristopher Kram <kram.kristopher@gmail.com>
2021-02-25 17:53:15 +0100

    Mars knapp

--- main.js
+++ main.js
@@ -61,15 +61,57 @@ document.getElementById("marsBtn").onclick = async function () {
   for (let i = 0; i < sol_keys.length; i++) {
     const sol = sol_keys[i];
 
-    console.log(sol);
-    console.log("atmospheric temperature, degrees Celsius");
-    console.log(obj[sol].AT.av);
-    console.log(obj[sol].PRE.av);
-    console.log(obj[sol].HWS.av);
-    console.log(obj[sol].WD.most_common.ct);
-    console.log(obj[sol].Season);
-    console.log(obj[sol].First_UTC);
-    console.log(obj[sol].Last_UTC);
+    console.log(`sol nr: ${sol}`);
+
+    if (obj[sol].AT != undefined) {
+      console.log(obj[sol].AT.av);
+      document.getElementById("MW-AT").innerText = obj[sol].AT.av;
+    } else {
+      document.getElementById("MW-AT").innerText = "N/A";
+      console.log("AT: NULL");
+    }
+    if (obj[sol].PRE != undefined) {
+      console.log(obj[sol].PRE.av);
+      document.getElementById("MW-PRE").innerText = obj[sol].PRE.av;
+    } else {
+      document.getElementById("MW-PRE").innerText = "N/A";
+      console.log("PRE: NULL");
+    }
+    if (obj[sol].HWS != undefined) {
+      console.log(obj[sol].HWS.av);
+      document.getElementById("MW-HWS").innerText = obj[sol].HWS.av;
+    } else {
+      document.getElementById("MW-HWS").innerText = "N/A";
+      console.log("HWS: NULL");
+    }
+    if (obj[sol].WD.most_common != undefined) {
+      console.log(obj[sol].WD.most_common.ct);
+      document.getElementById("MW-WD").innerText = obj[sol].WD.ct;
+    } else {
+      document.getElementById("MW-WD").innerText = "N/A";
+      console.log("WD: NULL");
+    }
+    if (obj[sol].Season != undefined) {
+      console.log(obj[sol].Season);
+      document.getElementById("MW-season").innerText = obj[sol].Season;
+    } else {
+      document.getElementById("MW-season").innerText = "N/A";
+      console.log("Season: NULL");
+    }
+    if (obj[sol].First_UTC != undefined) {
+      console.log(obj[sol].First_UTC);
+      document.getElementById("MW-firstUtc").innerText = obj[sol].First_UTC;
+    } else {
+      document.getElementById("MW-firstUtc").innerText = "N/A";
+      console.log("First_UTC: NULL");
+    }
+    if (obj[sol].Last_UTC != undefined) {
+      console.log(obj[sol].Last_UTC);
+      document.getElementById("MW-AT").innerText = obj[sol].Last_UTC;
+    } else {
+      document.getElementById("MW-lastUtc").innerText = "N/A";
+      console.log("Last_UTC: NULL");
+    }
 
     /*
     ●JSO[sol].AT.av - atmospheric temperature, degrees Celsius
