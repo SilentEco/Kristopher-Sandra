cdb340d6d2c60675b471e7bb5dcb443c0e1faeaa
Sandra Lindberg <70195807+SandraLindberg1@users.noreply.github.com>
2021-03-01 14:25:04 +0100

    fick iaf den till att få kod 200 på hämtningen av API

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
-document.querySelector("button").onclick = async function () {
+/* Min API Key dWzZWJW6v4s6mdpvB49DzO6iF7eeM4wRw7ZFsQZf */
+document.getElementById("notificationsFromNasa").onclick = function (){
+  fetch ("https://api.nasa.gov/DONKI/notifications?startDate=2014-05-01&endDate=2014-05-08&type=all&api_key=dWzZWJW6v4s6mdpvB49DzO6iF7eeM4wRw7ZFsQZf")
+ 
+  .then(response => {
+    return response.json();
+  })
+
+  .then(notifications =>{
+    console.log(notifications);
+  });
+  
+  let imgUrl = obj.hdurl;
+  let imgExplanation = obj.explanation
+  document.getElementById("spacePics").src = imgUrl;
+  document.getElementById("picExplanation").innerText = imgExplanation;
+};
+
+
+document.getElementById("dailyPicture").onclick = async function () {
   let url = new URL(
     "https://api.nasa.gov/planetary/apod?api_key=PCIlTkyhqe1nkB34QyF9XmZzzAj0RgkrFySr1uac"
   );
-  /*
-  let xhr = new XMLHttpRequest();
+
+  let response;
+  try {
+    response = await fetch(url);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+  let obj = await response.json();
+
+  let imgUrl = obj.hdurl;
+  let imgExplanation = obj.explanation;
+
+  document.getElementById("spacePics").src = imgUrl;
+  document.getElementById("picExplanation").innerText = imgExplanation;
+}; 
+ 
+ /*let xhr = new XMLHttpRequest();
   xhr.open("GET", url);
   xhr.responseType = "json";
 
@@ -25,19 +60,3 @@ document.querySelector("button").onclick = async function () {
   };
   xhr.send();
 */
-
-  let response;
-  try {
-    response = await fetch(url);
-  } catch (error) {
-    console.log(error);
-    return;
-  }
-  let obj = await response.json();
-
-  let imgUrl = obj.hdurl;
-  let imgExplanation = obj.explanation;
-
-  document.getElementById("spacePics").src = imgUrl;
-  document.getElementById("picExplanation").innerText = imgExplanation;
-};
